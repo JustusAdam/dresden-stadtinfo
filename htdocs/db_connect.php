6ba8d8c2e1b6530e499ca341ac58455c1ab45e01
JustusAdam <justusadam@justusadam-VPCCB15FD.(none)>
2013-11-05 14:56:17 +0100

    fixed db_connect

--- htdocs/db_connect.php
+++ htdocs/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$db = mysqli_connect("localhost", "test-user", "test", "project1");
+	$db = mysqli_connect("localhost", "test", "test", "project1");
 	if(!$db){
 		exit("Error: ".mysqli_connect_error());
 	}
